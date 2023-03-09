bc49859aa7d43974ab94c1404a077cf1b7efffde
bad4iz <developer@bad4iz.ru>
2023-03-09 20:13:47 +0400

    LSP init

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -50,7 +50,6 @@ return packer.startup(function(use)
   -- Colorschemes
   use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   -- use "lunarvim/darkplus.nvim"
-  
 
 
   -- cmp plugins
@@ -59,12 +58,21 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-path" -- path completions
   use "hrsh7th/cmp-cmdline" -- cmdline completions
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/cmp-nvim-lua"
+
+
 
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
 
+    -- LSP
+  use "neovim/nvim-lspconfig" -- enable LSP
+  use "williamboman/mason.nvim" -- simple to use language server installer
+  use "williamboman/mason-lspconfig.nvim" -- simple to use language server installer
+  use 'jose-elias-alvarez/null-ls.nvim' -- LSP diagnostics and code actions
   
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
