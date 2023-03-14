63003b54a978a5296fc9c7fa1133d89bdbf38a58
bad4iz <developer@bad4iz.ru>
2023-03-14 23:20:39 +0400

    key map default которые есть

--- lua/user/tabnine.lua
+++ lua/user/tabnine.lua
@@ -11,3 +11,19 @@ tabnine.setup({
   suggestion_color = {gui = "#808080", cterm = 244},
   exclude_filetypes = {"TelescopePrompt"}
 })
+-- local status_ok, tabnine = pcall(require, "cmp_tabnine.config")
+-- if not status_ok then
+--   return
+-- end
+--
+-- tabnine.setup {
+--   max_lines = 1000,
+--   max_num_results = 20,
+--   sort = true,
+--   run_on_every_keystroke = true,
+--   snippet_placeholder = "..",
+--   ignored_file_types = { -- default is not to ignore
+--     -- uncomment to ignore in lua:
+--     -- lua = true
+--   },
+-- }
